05da648457086e642b9b7d27dc847da73e37daa5
Kevin <kwang@bitglass.com>
2020-08-16 22:03:49 -0700

    Updates

--- sw.js
+++ sw.js
@@ -27,7 +27,7 @@ workbox.core.clientsClaim();
  */
 self.__precacheManifest = [
   {
-    "url": "webpack-runtime-db1e3be9f05e3f26bc85.js"
+    "url": "webpack-runtime-2b497337de63a5ad249c.js"
   },
   {
     "url": "styles.6debf6fae984d69492db.css"
@@ -46,7 +46,7 @@ self.__precacheManifest = [
   },
   {
     "url": "offline-plugin-app-shell-fallback/index.html",
-    "revision": "d9f03122cc66c95dac734ec5c5496306"
+    "revision": "989c98282953bcfdca8d51a55433ab8b"
   },
   {
     "url": "component---cache-caches-gatsby-plugin-offline-app-shell-js-c9875e2473a7c518755f.js"
